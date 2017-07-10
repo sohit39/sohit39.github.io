11405f850259152bee18cba75b3546a0fc302396
sohit39 <gatiganti44914@sas.edu.sg>
2017-07-11 00:04:03 +0800

    fade transition update

--- myScripts.js
+++ myScripts.js
@@ -6,10 +6,10 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom - 100 < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      if (objectBottom - 50 < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
       } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        if ($(this).css("opacity")==1) {$(this).fadeTo(800,0);}
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
@@ -23,10 +23,10 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom - 100 < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      if (objectBottom - 50 < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
       } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        if ($(this).css("opacity")==1) {$(this).fadeTo(800,0);}
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
@@ -40,10 +40,10 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom - 100< windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      if (objectBottom - 50< windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
       } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        if ($(this).css("opacity")==1) {$(this).fadeTo(800,0);}
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
@@ -57,10 +57,10 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom - 100< windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      if (objectBottom - 50< windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
       } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        if ($(this).css("opacity")==1) {$(this).fadeTo(800,0);}
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
