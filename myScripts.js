94cc9e5cf8879b2564ed4074c17e24572f79df2b
sohit39 <gatiganti44914@sas.edu.sg>
2017-07-10 23:53:26 +0800

    scrollfix

--- myScripts.js
+++ myScripts.js
@@ -6,7 +6,7 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectBottom - 100 < windowBottom) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
       } else { //object goes out of view (scrolling up)
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
@@ -23,7 +23,7 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectBottom - 100 < windowBottom) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
       } else { //object goes out of view (scrolling up)
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
@@ -40,7 +40,7 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectBottom - 100< windowBottom) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
       } else { //object goes out of view (scrolling up)
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
@@ -57,7 +57,7 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectBottom - 100< windowBottom) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
       } else { //object goes out of view (scrolling up)
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
